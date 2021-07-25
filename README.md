7f97be869f99fc00d4665ae904c7fba01a36f1b5
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-25 10:51:49 +1000

    refactor: eval command and README.md

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # North
 
-[![CD](https://github.com/hidden-umbrella/north/actions/workflows/CD.yml/badge.svg)](https://github.com/hidden-umbrella/north/actions/workflows/CD.yml)
+[![Okteto Push](https://github.com/hidden-umbrella/north/actions/workflows/okteto-push.yml/badge.svg)](https://github.com/hidden-umbrella/north/actions/workflows/okteto-push.yml)
 [![Depfu](https://badges.depfu.com/badges/02ff61f39590258ae458a6a3b9d42441/count.svg)](https://badges.depfu.com/github/hidden-umbrella/north?project_id=29655)
 
 ![GitHub issues](https://img.shields.io/github/issues-raw/hidden-umbrella/north)
@@ -19,10 +19,10 @@
 - Install yarn with `npm i -g yarn`
 - Install required dependencies with `yarn`
 - Build with `yarn build`
-- Configure the bot by coping or renaming main.example.json in the config folder to main.json and fill out all the blanks
+- Configure the bot by coping or rename main.example.json in the config folder to main.json and fill out all the blanks
 - You are now finished! Run the bot with `node .`
 
-With problems with self hosting DM me ArtieFuzzz#8298
+Problems with self hosting DM me ArtieFuzzz#8298
 
 ## License
 
--- src/commands/Owner/eval.ts
+++ src/commands/Owner/eval.ts
@@ -3,23 +3,25 @@ import { Message, MessageEmbed } from 'discord.js'
 import { ApplyOptions } from '@sapphire/decorators'
 import config from '../../config'
 import { Args } from '@sapphire/framework'
+import c from '@aero/centra'
 import { inspect } from 'util'
 
 @ApplyOptions<NorthCommandOptions>({
 	name: 'eval',
 	description: 'Evaluate javascript on the bot process',
 	hidden: true,
-	flags: ['silent', 's'],
+	flags: ['silent', 's', 'async'],
 	options: ['depth']
 })
 export default class Eval extends NorthCommand {
 	public async run (message: Message, args: Args): Promise<Message> {
 		if (!this.isOwner(message.author.id)) return await message.channel.send('You\'re not allowed to execute this command.')
 
-		let silent: boolean, depth: string, exp: string
+		let silent: boolean, depth: string, exp: string, async: boolean
 
 		exp = await args.rest('string')
 		silent = args.getFlags('silent', 's')
+		async = args.getFlags('async')
 		depth = args.getOption('depth')
 
 		/**
@@ -28,18 +30,18 @@ export default class Eval extends NorthCommand {
 		 */
 		const embed = new MessageEmbed()
 			.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 }))
-		const { success, output, Type }: { success: boolean, output: string, Type: string} = await this.eval(message, exp, depth)
+		const { success, output, Type }: { success: boolean, output: string, Type: string} = await this.eval(exp, depth, async)
 
 		if (silent) return null
 
-		/* if (output.length > 1000) {
+		if (output.length > 1000) {
 			const { key }: { key: string } = await c('https://hastebin.com', 'POST')
 				.path('documents')
 				.body(output)
 				.json()
 
 			return await message.channel.send(`The output was longer than 2000 characters: https://hastebin.com/${key}`)
-		} */
+		}
 
 		if (success) {
 			embed.setTitle('Success! | Result')
@@ -58,7 +60,7 @@ export default class Eval extends NorthCommand {
 		}
 	}
 
-	private async eval (message: Message, expression: string, depthOption?: string, asyncFlag?: boolean): Promise<{ success: boolean, output: string, Type: string }> {
+	private async eval (expression: string, depthOption?: string, asyncFlag?: boolean): Promise<{ success: boolean, output: string, Type: string }> {
 		let success: boolean, output: string, Type: string
 		try {
 			if (asyncFlag) expression = `(async () => {\n${expression}\n})()`
@@ -69,8 +71,7 @@ export default class Eval extends NorthCommand {
 			success = true
 		} catch (err) {
 			if (!Type) Type = typeof err
-			// eslint-disable-next-line @typescript-eslint/prefer-optional-chain
-			if (err && err.stack) this.container.logger.error('[Eval] ERROR!:', err.stack)
+			if (err?.stack) this.container.logger.error('[Eval] ERROR!:', err.stack)
 			output = err
 			success = false
 		}
