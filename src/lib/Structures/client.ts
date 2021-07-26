0e6db5a3ed3f1301934045616804e4a32569fc29
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-26 16:06:22 +1000

    revert: revert Music

--- src/lib/Structures/client.ts
+++ src/lib/Structures/client.ts
@@ -1,15 +1,12 @@
 import { SapphireClient, SapphireClientOptions } from '@sapphire/framework'
 import { KSoftClient } from '@aero/ksoft'
-import MusicManager from '../Music/Manager'
 import cfg from '../../config'
 
 export default class Client extends SapphireClient {
 	ksoft: KSoftClient
-	music: MusicManager
 	constructor (options: SapphireClientOptions) {
 		super(options)
 
-		this.music = new MusicManager(this)
 		this.ksoft = new KSoftClient(cfg.ksoft)
 	}
 
