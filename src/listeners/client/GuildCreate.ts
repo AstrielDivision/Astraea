1a7f9be22002e9923e25658e005f69a3038e5d70
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-27 21:23:02 +1000

    feat: added more Listeners
    Added Listeners: GuildCreate, GuildDelete and commandDenied

---
+++ src/listeners/client/GuildCreate.ts
@@ -0,0 +1,12 @@
+import { Events, Listener, ListenerOptions } from '@sapphire/framework'
+import { ApplyOptions } from '@sapphire/decorators'
+import { Guild } from 'discord.js'
+
+@ApplyOptions<ListenerOptions>({
+	event: Events.GuildCreate
+})
+export default class GuildCreate extends Listener {
+	public run (guild: Guild): void {
+		return this.container.logger.info('Joined', guild.name + ` (${guild.id})`)
+	}
+}
---
+++ src/listeners/client/GuildDelete.ts
@@ -0,0 +1,12 @@
+import { Events, Listener, ListenerOptions } from '@sapphire/framework'
+import { ApplyOptions } from '@sapphire/decorators'
+import { Guild } from 'discord.js'
+
+@ApplyOptions<ListenerOptions>({
+	event: Events.GuildDelete
+})
+export class GuildDelete extends Listener {
+	public run (guild: Guild): void {
+		return this.container.logger.info('Left', guild.name + ` (${guild.id})`)
+	}
+}
