3b2396abf20ddeb29ed08bb78e50c789b87c8cc3
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-28 19:38:21 +1000

    feat: 🎸 added thighdeology command
    refactored bigtiddy and hentai commands

--- src/commands/NSFW/thighdeology.ts
+++ src/commands/NSFW/thighdeology.ts
@@ -4,22 +4,23 @@ import { Message, MessageEmbed } from 'discord.js'
 import { RedditImage } from '@aero/ksoft'
 
 @ApplyOptions<NorthCommandOptions>({
-	name: 'bigtiddy',
-	aliases: ['booba'],
-	description: 'Returns a Image from r/bigtiddygothgf',
+	name: 'thigh',
+	aliases: ['thighs', 'thighdeology'],
+	description: 'Returns a Image from r/thighdeology',
 	cooldownLimit: 3,
+	nsfw: true,
 	cooldownDelay: 2000
 })
-export default class BigTiddy extends NorthCommand {
+export default class Thighdeology extends NorthCommand {
 	public async run (message: Message): Promise<Message> {
-		const { post, url }: RedditImage = await this.container.client.ksoft.images.reddit('bigtiddygothgf', { removeNSFW: false, span: 'day' })
+		const { url, post }: RedditImage = await this.container.client.ksoft.images.reddit('thighdeology', { span: 'day', removeNSFW: false })
 		const embed = new MessageEmbed()
 			.setTitle(post.title)
 			.setFooter(`Powered by api.ksoft.si ${post.author} | Upvotes: ${post.upvotes} | Downvotes ${post.downvotes}`)
 			.setURL(post.link)
+			.setColor('BLACK')
 			.setTimestamp()
 			.setImage(url)
-			.setColor('WHITE')
 		return await message.channel.send(embed)
 	}
 }
