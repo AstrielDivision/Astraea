cc6df5e87e3cc2522aa2278b022261984fd46f93
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-29 22:14:30 +1000

    feat: 🎸 Added a new NSFW command

---
+++ src/commands/NSFW/yiff.ts
@@ -0,0 +1,30 @@
+import { NorthCommand, NorthCommandOptions } from '../../lib/Structures/Command'
+import { ApplyOptions } from '@sapphire/decorators'
+import { Message, MessageEmbed } from 'discord.js'
+import Yiff from '../../lib/Structures/Yiff'
+import { Args } from '@sapphire/framework'
+
+@ApplyOptions<NorthCommandOptions>({
+	name: 'yiff',
+	aliases: ['yiff', 'floofydev'],
+	description: 'Returns an Image from api.floofy.dev with your selected tags',
+	cooldownLimit: 6,
+	nsfw: true,
+	cooldownDelay: 5000,
+	options: ['results', 'r']
+})
+export default class YiffFloofyDev extends NorthCommand {
+	public async run (message: Message, args: Args): Promise<Message> {
+		const req: Request = await Yiff.floofy()
+
+		const embed = new MessageEmbed()
+			.setImage(req.url)
+			.setColor('RANDOM')
+
+		return await message.channel.send(embed)
+	}
+}
+
+interface Request {
+	url: string
+}
