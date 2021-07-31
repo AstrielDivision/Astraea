c01256b73ae666ddf8de8f86716baf612dee58fe
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-31 13:32:09 +1000

    fix: 🐛 Fixed astrophotography command build error

--- src/commands/Images/astrophotography.ts
+++ src/commands/Images/astrophotography.ts
@@ -9,16 +9,8 @@ import { ApplyOptions } from '@sapphire/decorators'
 	cooldownLimit: 3,
 	cooldownDelay: 2000
 })
-export default class Astrophotography extends AstraeaCommand {
-	public async run (message: Message): Promise<Message> {
-		const { post, url }: RedditImage = await this.container.client.ksoft.images.reddit('astrophotography', { removeNSFW: true, span: 'day' })
-		const embed = new MessageEmbed()
-			.setTitle(post.title)
-			.setFooter(`Powered by api.ksoft.si ${post.author} | Upvotes: ${post.upvotes} | Downvotes ${post.downvotes}`)
-			.setURL(post.link)
-			.setTimestamp()
-			.setImage(url)
-			.setColor('DARK_GREEN')
-		return await message.channel.send(embed)
+export default class Memes extends AstraeaRedditCommand {
+	constructor (Context: PieceContext, options: AstraeaCommandOptions) {
+		super({ subreddit: 'astrophotography', nsfw: false, colour: 'BLACK' }, Context, options)
 	}
 }
--- src/commands/Images/blessed.ts
+++ src/commands/Images/blessed.ts
@@ -1,13 +1,15 @@
 import { AstraeaRedditCommand, AstraeaCommandOptions } from '../../lib/Structures/Command'
+import { PieceContext } from '@sapphire/framework'
 import { ApplyOptions } from '@sapphire/decorators'
 
 @ApplyOptions<AstraeaCommandOptions>({
 	name: 'blessed',
-	description: 'Returns an image from r/blessedimages'
+	description: 'Returns an image from r/blessedimages',
+	cooldownLimit: 3,
+	cooldownDelay: 2000
 })
-export default class Blessed extends AstraeaRedditCommand {
-	constructor (...args: unknown[]) {
-		// @ts-ignore
-		super({ subreddit: 'blessed', nsfw: false }, ...args)
+export default class Memes extends AstraeaRedditCommand {
+	constructor (Context: PieceContext, options: AstraeaCommandOptions) {
+		super({ subreddit: 'blessedimages', nsfw: false, colour: 'WHITE' }, Context, options)
 	}
 }
