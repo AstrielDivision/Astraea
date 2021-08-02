fc7787ffae95c4ac2dd8dcde031994110217c8af
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-08-03 09:10:31 +1000

    feat(commands): added beans and DiWHY reddit commands

---
+++ src/commands/Images/beans.ts
@@ -0,0 +1,14 @@
+import { AstraeaRedditCommand, AstraeaCommandOptions } from '../../lib/Structures/Command'
+import { PieceContext } from '@sapphire/framework'
+import { ApplyOptions } from '@sapphire/decorators'
+
+@ApplyOptions<AstraeaCommandOptions>({
+	name: 'beans',
+	description: 'Returns a Image from r/BeansInStrangePlaces',
+	nsfw: false
+})
+export default class BeansInStrangePlaces extends AstraeaRedditCommand {
+	constructor (Context: PieceContext, options: AstraeaCommandOptions) {
+		super({ subreddit: 'BeansInStrangePlaces', nsfw: false, colour: 'BROWN' }, Context, options)
+	}
+}
