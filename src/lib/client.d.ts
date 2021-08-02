0ad8e1d4518f2ad400b2c380e32913d632ad0660
Universal Studio <uni@un1.pw>
2021-08-02 08:16:48 +0200

    chore(types): extend client interface by statusUpdater field
    add field `statusUpdater` to SapphireClient overrides,
    to make it available for ts intellisense

--- src/lib/client.d.ts
+++ src/lib/client.d.ts
@@ -1,8 +1,10 @@
 import { KSoftClient } from '@aero/ksoft'
+import StatusUpdater from '@tmware/status-rotate'
 
 declare module '@sapphire/framework' {
 	interface SapphireClient {
 		ksoft: KSoftClient
+		statusUpdater: StatusUpdater
 	}
 	interface ILogger {
 		// eslint-disable-next-line @typescript-eslint/method-signature-style, @typescript-eslint/no-invalid-void-type
