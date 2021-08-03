d6b20d235023ce7e2cc945aa8a0291f12c9f3582
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-08-03 13:27:53 +1000

    refactor(corona-fetch): updated corona-fetch class methods

--- src/lib/corona-fetch/corona.ts
+++ src/lib/corona-fetch/corona.ts
@@ -5,15 +5,11 @@ import { All, Country } from './types'
 
 class CoronaFetch {
 	public async all (): Promise<All> {
-		const res: All = await request().catch((err: Error) => console.error(err))
-
-		return res
+		return await request().catch((err: Error) => console.error(err))
 	}
 
 	public async country (country: string): Promise<Country> {
-		const res: Country = await request(country).catch((err: Error) => console.error(err))
-
-		return res
+		return await request(country).catch((err: Error) => console.error(err))
 	}
 }
 
