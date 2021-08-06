653ce731d4c452d2386d5e47a4186288fef68306
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-08-06 11:01:54 +1000

    refactor(client): update client.d.ts

--- src/lib/client.d.ts
+++ src/lib/client.d.ts
@@ -1,10 +1,12 @@
 import { KSoftClient } from '@aero/ksoft'
 import StatusUpdater from '@tmware/status-rotate'
+import ClientUtils from './ClientUtils'
 
 declare module '@sapphire/framework' {
 	interface SapphireClient {
 		ksoft: KSoftClient
 		statusUpdater: StatusUpdater
+		util: ClientUtils
 	}
 	interface ILogger {
 		// eslint-disable-next-line @typescript-eslint/method-signature-style
