6547c0e5dc9c27a1c0c5502052447322f30ec61d
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-08-07 18:03:47 +1000

    feat(commands): added purge command

---
+++ src/commands/Moderation/purge.ts
@@ -0,0 +1,36 @@
+import { AstraeaCommand, AstraeaCommandOptions } from '../../lib/Structures/Command'
+import { Message, TextChannel } from 'discord.js'
+import { ApplyOptions, RequiresPermissions } from '@sapphire/decorators'
+import { Args } from '@sapphire/framework'
+
+@ApplyOptions<AstraeaCommandOptions>({
+	name: 'purge',
+	description: 'Makes the bot purge X number of messages',
+	requiredClientPermissions: ['MANAGE_MESSAGES'],
+	cooldownDelay: 2000,
+	cooldownLimit: 2,
+	preconditions: ['GuildTextOnly']
+})
+export default class Purge extends AstraeaCommand {
+	@RequiresPermissions('MANAGE_MESSAGES')
+	// eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+	public async run (message: Message, args: Args): Promise<void | Message> {
+		const amount = (await args.pickResult('number')).value
+
+		if (!amount) return await message.channel.send('You didn\'t provide an amount!')
+		if (amount > 100) return await message.channel.send('I can only purge 100 messages!')
+		if (amount < 1) return await message.channel.send('I can\'t just delete nothing :\\')
+
+		await message.delete()
+
+		const messages = await message.channel.messages.fetch({ limit: amount })
+
+		await (message.channel as TextChannel).bulkDelete(messages.array().slice(0, amount))
+
+		return await message.channel.send(`Purged **${amount}** messages`).then((msg) => {
+			setTimeout(() => {
+				void msg.delete()
+			}, 2000)
+		})
+	}
+}
