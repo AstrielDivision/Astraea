e0debdac550549932f47fb8ea5651ce7ecebf8e9
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-08-12 09:54:08 +1000

    refactor(misc): update owoify command
    Instead of using owoify-js we're now using owofire

--- src/commands/Misc/owoify.ts
+++ src/commands/Misc/owoify.ts
@@ -2,44 +2,21 @@ import { AstraeaCommand, AstraeaCommandOptions } from '../../lib/Structures/Comm
 import { Message } from 'discord.js'
 import { ApplyOptions } from '@sapphire/decorators'
 import { Args } from '@sapphire/framework'
-import owoify from 'owoify-js'
+import owoify from 'owofire'
 
 @ApplyOptions<AstraeaCommandOptions>({
 	name: 'owoify',
 	description: 'owoify your text',
-	flags: ['uwu', 'uvu'],
-	usage: '<text> [--uwu | --uvu]'
+	usage: '<text>'
 })
 export default class OwOify extends AstraeaCommand {
 	public async run (message: Message, args: Args): Promise<Message> {
-		const text = (await args.pickResult('string')).value
-		const uvuFlag = args.getFlags('uvu')
-		const uwuFlag = args.getFlags('uwu')
+		const text = (await args.restResult('string')).value
 
 		if (!text) {
 			return await message.channel.send('No text provided!')
 		}
 
-		if (uvuFlag) return await this.uvu(message, text)
-		if (uwuFlag) return await this.uwu(message, text)
-		return await this.owo(message, text)
-	}
-
-	private async uvu (message: Message, text: string): Promise<Message> {
-		const uvuifed = owoify(text, 'uvu')
-
-		return await message.channel.send(uvuifed)
-	}
-
-	private async uwu (message: Message, text: string): Promise<Message> {
-		const uwuifed = owoify(text, 'uwu')
-
-		return await message.channel.send(uwuifed)
-	}
-
-	private async owo (message: Message, text: string): Promise<Message> {
-		const owoified = owoify(text)
-
-		return await message.channel.send(owoified)
+		return await message.channel.send(owoify(text))
 	}
 }
