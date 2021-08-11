cf01945854841009f1af0d7957eb54fae5957c0e
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-08-11 18:04:24 +1000

    refactor(cryptography): update hash description

--- src/commands/Cryptography/hash.ts
+++ src/commands/Cryptography/hash.ts
@@ -7,8 +7,8 @@ import cryptoJS from 'crypto-js'
 
 @ApplyOptions<AstraeaCommandOptions>({
 	name: 'hash',
-	description: 'Hash your text in sha1',
-	detailedDescription: 'Hash your text in either sha1, sha256, sha512 or md5',
+	description: 'Hash your text',
+	detailedDescription: 'Hash your text in either sha1, sha256, sha512, md5 or ripemd',
 	flags: ['sha1', 'sha256', 'sha512', 'md5', 'all', 'ripemd']
 })
 export default class Hash extends AstraeaCommand {
