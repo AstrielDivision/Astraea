6d0b5d6d4838d69c45edfb6300e29f6b3dd8214e
Universal Studio <uni@un1.pw>
2021-08-11 17:04:06 +0200

    fix(crypto): check permissions before deleting invoking message
    check for client's `MANAGE_MESSAGES` permission before deleting message
    
    also clean up crypto commands for improved readability and less duplicate code
    
    Closes #23

--- src/commands/Cryptography/rc4.ts
+++ src/commands/Cryptography/rc4.ts
@@ -1,8 +1,8 @@
-import { AstraeaCommand, AstraeaCommandOptions } from '../../lib/Structures/Command'
-import { Message } from 'discord.js'
 import { ApplyOptions } from '@sapphire/decorators'
 import { Args } from '@sapphire/framework'
 import crypto from 'crypto-js'
+import { Message, Permissions } from 'discord.js'
+import { AstraeaCommand, AstraeaCommandOptions } from '../../lib/Structures/Command'
 
 @ApplyOptions<AstraeaCommandOptions>({
 	name: 'rc4',
@@ -14,15 +14,18 @@ import crypto from 'crypto-js'
 })
 export default class RC4Drop extends AstraeaCommand {
 	public async run (message: Message, args: Args): Promise<Message> {
-		const decryptFlags = args.getFlags('d', 'decrypt')
+		const decryptFlag = args.getFlags('d', 'decrypt')
 		const text = (await args.restResult('string')).value
 		const secret = args.getOption('s', 'secret')
 
 		if (!text) return await message.channel.send('No text provided')
 		if (!secret) return await message.channel.send('No secret provided. (Hint: Use -s=<randomLetters> or --secret=<randomLetters>)')
 
-		if (decryptFlags) return await this.decrypt(message, text, secret).catch(async () => await message.channel.send('Couldn\'t decrypt this text!'))
-		return await this.encrypt(message, text, secret)
+		if (message.guild.me.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)) void message.delete()
+
+		const result = decryptFlag ? this.decrypt(text, secret) : this.encrypt(text, secret)
+
+		return await message.channel.send(result)
 	}
 
 	/**
@@ -30,12 +33,8 @@ export default class RC4Drop extends AstraeaCommand {
 	 * Secret: ABC
 	 * Output: U2FsdGVkX1/64CRgHLq4o4+2uPg=
 	 */
-	private async encrypt (message: Message, input: string, secret: string): Promise<Message> {
-		await message.delete()
-
-		const encrypted = crypto.RC4Drop.encrypt(input, secret).toString()
-
-		return await message.channel.send(encrypted)
+	private encrypt (input: string, secret: string): string {
+		return crypto.RC4Drop.encrypt(input, secret).toString()
 	}
 
 	/**
@@ -43,11 +42,7 @@ export default class RC4Drop extends AstraeaCommand {
 	 * Secret: ABC
 	 * Output: ABC
 	 */
-	private async decrypt (message: Message, input: string, secret: string): Promise<Message> {
-		await message.delete()
-
-		const decrypted = crypto.RC4Drop.decrypt(input, secret).toString(crypto.enc.Utf8)
-
-		return await message.channel.send(decrypted)
+	private decrypt (input: string, secret: string): string {
+		return crypto.RC4Drop.decrypt(input, secret).toString(crypto.enc.Utf8)
 	}
 }
