581ac59fb7e031276d1a1dde144dd25f38331415
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-08-14 18:39:24 +1000

    feat(util-commands): added userinfo command

---
+++ src/commands/Util/userinfo.ts
@@ -0,0 +1,40 @@
+import { AstraeaCommand, AstraeaCommandOptions } from '../../lib/Structures/Command'
+import { Message, GuildMember, MessageEmbed } from 'discord.js'
+import { ApplyOptions } from '@sapphire/decorators'
+import { Args } from '@sapphire/framework'
+import dayjs from 'dayjs'
+
+@ApplyOptions<AstraeaCommandOptions>({
+  name: 'userinfo',
+  aliases: ['ui'],
+  description: 'Fetch a discord user\'s info.',
+  usage: '[userID | @user]'
+})
+export default class example extends AstraeaCommand {
+  public async run(message: Message, args: Args): Promise<Message> {
+    const member = (await args.pickResult('member')).value
+
+    return await this.Info(message, member ?? message.member)
+  }
+
+  private async Info(message: Message, member: GuildMember): Promise<Message> {
+    const KSoftBan = await this.container.client.ksoft.bans.check(member.user.id)
+    const isBot = member.user.bot
+
+    const embed = new MessageEmbed()
+      .setTitle(`${member.user.tag}'s Info`)
+      .setColor('BLURPLE')
+      .setTimestamp()
+      .setThumbnail(member.user.avatarURL({ dynamic: true }))
+      .addFields(
+        { name: 'Discrim', value: member.user.discriminator, inline: true },
+        { name: 'Joined', value: dayjs(member.joinedAt).format('MM/DD/YYYY'), inline: true },
+        { name: 'Registered', value: dayjs(member.user.createdAt).format('MM/DD/YYYY'), inline: true },
+        { name: 'Banned on KSoft?', value: KSoftBan ? 'Yes' : 'No', inline: true },
+        { name: 'Is a Bot?', value: isBot ? 'Yes' : 'No', inline: true }
+      )
+      .setFooter(`ID: ${member.user.id}`)
+
+    return await message.channel.send({ embeds: [embed] })
+  }
+}
