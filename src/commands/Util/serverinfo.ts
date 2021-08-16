170a602565b61645f28a21812268eb43ee43e578
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-08-16 10:17:02 +1000

    refactor(util-commands): update serverinfo embed fields

--- src/commands/Util/serverinfo.ts
+++ src/commands/Util/serverinfo.ts
@@ -23,20 +23,28 @@ export default class example extends AstraeaCommand {
       .setTitle(`${guild.name} | Guild Info`)
       .setColor('BLURPLE')
       .addFields(
-        { name: 'Guild ID', value: guild.id, inline: true },
-        { name: 'Owner ID', value: (await guild.fetchOwner()).id, inline: true },
-        { name: 'Owner', value: (await guild.fetchOwner()).displayName, inline: true },
+        { name: 'Owner', value: `<@!${(await guild.fetchOwner()).id}>`, inline: true },
         { name: 'Channels', value: guild.channels.cache.size.toString(), inline: true },
         { name: 'Member Count', value: guild.memberCount.toString(), inline: true },
         { name: 'Created At', value: dayjs(guild.createdTimestamp).format('MM/DD/YYYY'), inline: true },
         {
           name: `Roles (${guild.roles.cache.size})`,
-          value: roles.length > 10 ? roles.join(', ') : roles.join(', ').slice(0, 10),
+          value: roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.trimArray(roles).join(', ') : 'None',
           inline: true
         }
       )
       .setThumbnail(guild.iconURL({ dynamic: true }))
 
     return await message.channel.send({ embeds: [embed] })
+  }
+
+  private trimArray(arr: string[], max = 10): string[] {
+    if (arr.length > max) {
+      const leng = arr.length - max
+      arr = arr.slice(0, max)
+      arr.push(`${leng} more...`)
+    }
+
+    return arr
   }
 }
