1b5b7b78ab8d0d688e6c8f361d2c444a35182cd8
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-08-18 10:40:44 +1000

    refactor(moderation): update warn.ts
    The user can't just warn themselfs

--- src/commands/Moderation/pardon.ts
+++ src/commands/Moderation/pardon.ts
@@ -1,16 +1,26 @@
-// TODO
 import { AstraeaCommand, AstraeaCommandOptions } from '#lib/Structures/BaseCommand'
 import type { Message } from 'discord.js'
 import { ApplyOptions } from '@sapphire/decorators'
 import type { Args } from '@sapphire/framework'
+import CaseModel from '#lib/Models/WarnCase'
 
 @ApplyOptions<AstraeaCommandOptions>({
-  name: 'example',
-  description: 'Solve this',
-  usage: '[optional] <required>'
+  description: 'Pardon a warn case of a user',
+  usage: '<@user | userID> <caseID>'
 })
-export default class example extends AstraeaCommand {
+export default class Pardon extends AstraeaCommand {
   public async run(message: Message, args: Args): Promise<Message> {
-    return await message.channel.send('Not fully ready')
+    const { user } = (await args.pickResult('member')).value
+    const caseID = (await args.pickResult('string')).value
+
+    if (!user) return await message.channel.send('No user provided!')
+    if (!caseID) return await message.channel.send('No case ID provided!')
+
+    await CaseModel.findOneAndUpdate(
+      { user_id: user.id, guild: message.guild.id, case_id: caseID },
+      { pardoned: true }
+    ).catch(() => void message.channel.send(`A case with the ID ${caseID} doesn't exist.`))
+
+    return await message.channel.send(`<@${user.id}>'s case has been pardoned`)
   }
 }
