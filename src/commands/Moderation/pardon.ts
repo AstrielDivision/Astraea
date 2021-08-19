0351db5a25038119a3c4c31f66ed977ffe3e5a53
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-08-19 12:23:07 +1000

    refactor(moderation): mods and admins can remove warnings now

--- src/commands/Moderation/pardon.ts
+++ src/commands/Moderation/pardon.ts
@@ -6,23 +6,32 @@ import CaseModel from '#lib/Models/WarnCase'
 
 @ApplyOptions<AstraeaCommandOptions>({
   description: 'Pardon a warn case of a user',
-  usage: '<@user | userID> <caseID>'
+  usage: '<@user | userID> <caseID>',
+  flags: ['remove', 'r']
 })
 export default class Pardon extends AstraeaCommand {
   @RequiresUserPermissions('KICK_MEMBERS')
   public async run(message: Message, args: Args): Promise<Message> {
     const { user } = (await args.pickResult('member')).value
     const caseID = (await args.pickResult('string')).value
+    const removeFlag = args.getFlags('remove', 'r')
 
     if (!user) return await message.channel.send('No user provided!')
     if (!caseID) return await message.channel.send('No case ID provided!')
 
     if (await this.isPardoned(user, message.guild, caseID)) return await this.unPardon(message, user, caseID)
 
-    return await this.pardon(message, user, caseID)
+    return await this.pardon(message, user, caseID, removeFlag)
   }
 
-  private async pardon(message: Message, user: User, caseID: string): Promise<Message> {
+  private async pardon(message: Message, user: User, caseID: string, remove: boolean): Promise<Message> {
+    if (remove) {
+      await CaseModel.findOneAndRemove({ user_id: user.id, guild: message.guild.id, case_id: caseID }).catch(
+        () => void message.channel.send(`A case with the ID ${caseID} doesn't exist.`)
+      )
+
+      return await message.channel.send(`${user.toString()}'s case has been pardoned and removed (irreversible)`)
+    }
     await CaseModel.findOneAndUpdate(
       { user_id: user.id, guild: message.guild.id, case_id: caseID },
       { pardoned: true }
