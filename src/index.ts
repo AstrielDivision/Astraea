b8cfbe41c0ab694b87e47cd5e59820d87858b49d
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-08-24 09:52:58 +1000

    feat(index): fetchPrefix

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import Client from './lib/Structures/client'
 import Logger from './lib/Structures/Logger'
 import cfg from './config'
+import type { Message } from 'discord.js'
+import db from '#database'
+import type { GuildSettings } from '#types'
 
 const client = new Client({
   defaultPrefix: cfg.prefix,
   caseInsensitivePrefixes: true,
   caseInsensitiveCommands: true,
   logger: { instance: new Logger('Astraea') },
-  intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_BANS', 'GUILD_WEBHOOKS', 'GUILD_MEMBERS', 'GUILD_MESSAGE_REACTIONS']
+  intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_BANS', 'GUILD_WEBHOOKS', 'GUILD_MEMBERS', 'GUILD_MESSAGE_REACTIONS'],
+  fetchPrefix: async (message: Message) => {
+    const { data: settings } = await db.from<GuildSettings>('guilds').select().eq('guild_id', message.guild.id).single()
+
+    return settings.prefix ?? cfg.prefix
+  }
 })
 
 void client.start()
