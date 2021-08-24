8b18af72d3bf9307e908ae6e40e4e7e36dda191e
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-08-24 17:25:16 +1000

    refactor(listeners): remove guild settings on leave

--- src/listeners/client/GuildDelete.ts
+++ src/listeners/client/GuildDelete.ts
@@ -1,12 +1,15 @@
 import { Events, Listener, ListenerOptions } from '@sapphire/framework'
 import { ApplyOptions } from '@sapphire/decorators'
 import type { Guild } from 'discord.js'
+import db from '#database'
+import type { GuildSettings } from '#types'
 
 @ApplyOptions<ListenerOptions>({
   event: Events.GuildDelete
 })
 export default class GuildDelete extends Listener {
-  public run(guild: Guild): void {
+  public async run(guild: Guild): Promise<void> {
+    await db.from<GuildSettings>('guilds').delete().eq('guild_id', guild.id)
     return this.container.logger.info(`Left ${guild.name} (${guild.id})`)
   }
 }
